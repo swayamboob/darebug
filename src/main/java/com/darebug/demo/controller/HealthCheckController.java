f4da1d91250d913eea94fe413613e5845910fb0d
DareBug <swayamboob@gmail.com>
2026-04-19 20:36:19 +0530

    initialized project

---
+++ src/main/java/com/darebug/demo/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.darebug.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "HelloWorld";
+    }
+}
